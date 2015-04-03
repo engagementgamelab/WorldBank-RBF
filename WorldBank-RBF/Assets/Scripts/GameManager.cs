68f17d1404196018a0dd301260bbc9bdb80e8b88
Johnny Richardson <john@thejohnshow.com>
2015-04-03 17:05:13 -0400

    Created basic networking management and file download/saving.

---
+++ WorldBank-RBF/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// This should live in a static global dictionary somewhere
+		string gameData = NetworkManager.Instance.DownloadDataFromURL("http://localhost:3000/api/gameData");
+
+		// create file in Assets/Config/
+		File.WriteAllText(Application.dataPath + "/Config/data.json", gameData); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ WorldBank-RBF/Assets/Scripts/Utilities/Network/NetworkManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.IO;
+using SimpleJSON;
+ 
+// TODO: This needs lots of cleanup
+public class NetworkManager : MonoBehaviour {
+    protected NetworkManager() {}
+    private static NetworkManager _instance = null;
+        
+    public static NetworkManager Instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType (typeof (NetworkManager)) as NetworkManager;
+
+                if (_instance == null) {
+                    GameObject obj = new GameObject ();
+                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    _instance = obj.AddComponent<NetworkManager> ();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public void GetURL (string url, string enumCallback) {
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+        
+    }
+
+    public string DownloadDataFromURL(string url) {
+        WebClient client = new WebClient();
+
+        Stream data = client.OpenRead(url);
+        StreamReader reader = new StreamReader(data);
+        
+        return reader.ReadToEnd();
+    }
+
+    public void GetJSON (string url) {
+        GetURL(url, "GetJSONResponse");
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+     {
+         yield return www;
+     
+         // check for errors
+         if (www.error == null)
+         {
+             Debug.Log("WWW Ok!: " + www.text);
+         } else {
+             Debug.Log("WWW Error: "+ www.error);
+         } 
+     }
+
+
+    IEnumerator GetJSONResponse(WWW www)
+     {
+         yield return www;
+     
+         // check for errors
+         if (www.error == null)
+         {
+             JSONNode response = JSON.Parse(www.text)["content"]["phase_one"];
+             string strRes = response.Value;
+
+             Debug.Log("WWW Ok!: " + strRes);
+         } else {
+             Debug.Log("WWW Error: "+ www.error);
+         }    
+     }
+}
