e9450f3b61fa4319311af98814adb4b6a75eac31
Johnny Richardson <john@thejohnshow.com>
2015-04-20 10:39:10 -0400

    Added missing files

---
+++ WorldBank-RBF/Assets/Scripts/NPCBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Timers;
+
+public class NPCBehavior : MB {
+
+	public DataManager.NPC npcRef;
+	public GameObject diagManager;
+
+	private Transform targetNPC;
+ 	private Timer aTimer;
+
+ 	private bool startDialog = false;
+ 	private float cameraStartTime = 0.7f;
+ 	private float cameraTime = 0.7f;
+
+ 	public void Initialize(NPC npcData, GameObject manager) {
+
+
+
+ 	}
+
+ 	// On Touch/Click NPC
+	void OnMouseDown() {
+		CameraBehavior.ZoomIn(transform);
+		
+		startDialog = true;
+		cameraStartTime = 0;
+	}
+
+	void Update()
+	{
+
+		if(!startDialog) 
+			return;
+
+		if(cameraStartTime < cameraTime)
+		    cameraStartTime += Time.deltaTime;
+		else {
+			ExecuteEvents.Execute<INPC>(diagManager, null, (x,y)=>x.OnNPCSelected(npcRef));	 
+			startDialog = false;
+		}
+	}
+}
