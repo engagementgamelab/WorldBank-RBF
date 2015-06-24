1c9cf9e0c886883cffaad89b3601f8d9b88263e4
esalling23 <esalling23@gmail.com>
2015-06-24 15:42:06 -0400

    merge

---
+++ WorldBank-RBF/Assets/Scripts/PlanSelection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanSelection : MonoBehaviour {
+
+	public GameObject nextButton;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("hello world!");
+	}
+
+}
