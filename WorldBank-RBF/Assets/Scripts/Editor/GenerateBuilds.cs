d368cd9c0f03127b142f993e9bfb742566e97cea
Johnny Richardson <john@thejohnshow.com>
2015-07-07 11:52:04 -0400

    Updated GenerateBuilds script to allow scenes to be specified.

--- WorldBank-RBF/Assets/Scripts/Editor/GenerateBuilds.cs
+++ WorldBank-RBF/Assets/Scripts/Editor/GenerateBuilds.cs
@@ -17,40 +17,44 @@ using System.Collections.Generic;
 
 class GenerateBuilds {
 
-    static string[] SCENES = FindEnabledEditorScenes();
+    // These are the scenes that the build server is going to use
+    static string[] SCENES = new string[] {"PhaseOne", "PhaseTwo"};
+
+    static string SCENE_PREFIX = "Assets/Scenes/";
+    static string SCENE_AFFIX = ".unity";
 
     static string APP_NAME = "WorldBank";
     static string TARGET_DIR = "Output";
 
     [MenuItem ("Build/Prepare Materials")]
     static void PrepareMaterials () {
-        MaterialsManager.PrepareMaterialsFromTextures ();
+        MaterialsManager.PrepareMaterialsFromTextures();
     }
 
     [MenuItem ("Build/Build Mac OS X Universal")]
     static void PerformMacOSXBuild ()
     {
-             string target_dir = APP_NAME + ".app";
-             GenericBuild(SCENES, TARGET_DIR + "/Mac/" + target_dir, BuildTarget.StandaloneOSXUniversal, BuildOptions.None);
+        string target_dir = APP_NAME + ".app";
+        GenericBuild("Mac", target_dir, BuildTarget.StandaloneOSXUniversal, BuildOptions.None);
     }
     [MenuItem ("Build/Build PC")]
     static void PerformPCBuild ()
     {
-             GenericBuild(SCENES, TARGET_DIR + "/PC/" + APP_NAME, BuildTarget.StandaloneWindows, BuildOptions.None);
+        GenericBuild("PC", APP_NAME, BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 
     [MenuItem ("Build/Build WebGL")]
     static void PerformWebGLBuild ()
     {
-             string target_dir = APP_NAME;
-             GenericBuild(SCENES, TARGET_DIR + "/WebGL/" + target_dir, BuildTarget.WebGL, BuildOptions.None);
+        string target_dir = APP_NAME;
+        GenericBuild("WebGL", target_dir, BuildTarget.WebGL, BuildOptions.None);
     }
 
     [MenuItem ("Build/Build Web")]
     static void PerformWebBuild ()
     {
-             string target_dir = APP_NAME;
-             GenericBuild(SCENES, TARGET_DIR + "/Web/" + target_dir, BuildTarget.WebPlayer, BuildOptions.None);
+         string target_dir = APP_NAME;
+         GenericBuild("Web", target_dir, BuildTarget.WebPlayer, BuildOptions.None);
     }
 
     [MenuItem ("Build/Build All")]
@@ -62,23 +66,28 @@ class GenerateBuilds {
         PerformWebBuild();
     }
 
-    private static string[] FindEnabledEditorScenes() {
+    static string[] FindEnabledScenes() {
+        
         List<string> EditorScenes = new List<string>();
-        foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
-            if (!scene.enabled) continue;
-            EditorScenes.Add(scene.path);
-        }
+        
+        foreach(string sceneName in SCENES)
+            EditorScenes.Add(SCENE_PREFIX + sceneName + SCENE_AFFIX);
+
         return EditorScenes.ToArray();
+
     }
 
-    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    static void GenericBuild(string platform, string buildDir, BuildTarget build_target, BuildOptions build_options)
     {
-            EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-            PrepareMaterials ();
-            string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
-            if (res.Length > 0) {
-                    throw new Exception("BuildPlayer failure: " + res);
-            }
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        PrepareMaterials ();
+        
+        string res = BuildPipeline.BuildPlayer(FindEnabledScenes(), TARGET_DIR + "/" + platform + "/" + buildDir, build_target, build_options);
+        
+        if (res.Length > 0)
+            throw new Exception("BuildPlayer failure: " + res);
+
     }
 
 }
