ea39493a61bcad04770516dc998b94c79c46d7ed
bret <bret@eee-2.local>
2015-07-29 12:37:52 -0400

    Message

---
+++ WorldBank-RBF/Assets/ThisDragHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ThisDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public static GameObject itemBeingDragged;
+	public static Vector3 startPosition;
+	public static Transform startParent;
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		itemBeingDragged = gameObject;
+		startPosition = transform.position;
+		startParent = transform.parent;
+
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+	
+	public void OnDrag (PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+
+		itemBeingDragged = null;
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+
+		if(transform.parent != startParent) {
+			transform.position = startPosition;
+
+			return;
+			}
+
+		if(transform.parent = startParent) {			
+			transform.position = startPosition;
+
+		}
+	}
+}
