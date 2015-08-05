0855f7f9b9398ffea6306cdd465d7b3a52acb55f
Johnny Richardson <john@thejohnshow.com>
2015-08-05 10:42:04 -0400

    Started merge of Bret's fixes.

--- WorldBank-RBF/Assets/Scripts/UI/PlanTactics/TacticButton.cs
+++ WorldBank-RBF/Assets/Scripts/UI/PlanTactics/TacticButton.cs
@@ -4,15 +4,15 @@ using System.Collections;
 
 public class TacticButton : MB {
 
-	Text text;
-	protected Text Text {
-		get {
-			if (text == null) {
-				text = Transform.GetChild (0).GetComponent<Text> ();
-			}
-			return text;
-		}
-	}
+//	Text text;
+//	protected Text Text {
+//		get {
+//			if (text == null) {
+//				text = Transform.GetChild (0).GetComponent<Text> ();
+//			}
+//			return text;
+//		}
+//	}
 
 	protected Column column;
 	protected Transform contentParent;
@@ -21,7 +21,7 @@ public class TacticButton : MB {
 		this.column = column;
 		this.contentParent = contentParent;
 		SetParent (contentParent);
-		Text.text = content;
+//		Text.text = content;
 	}
 
 	public virtual void OnClick () {}
