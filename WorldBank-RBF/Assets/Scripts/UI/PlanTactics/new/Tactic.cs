385ae4ad4c22b672e4b6edcc7d7533d84097058e
jayvachon <jaylvachon@gmail.com>
2015-09-03 19:40:20 -0400

    implement dragging of tactics

---
+++ WorldBank-RBF/Assets/Scripts/UI/PlanTactics/new/Tactic.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Tactic : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	Transform planPanel;
+	Transform PlanPanel {
+		get {
+			if (planPanel == null) {
+				planPanel = GameObject.Find ("PlanPanel").transform;
+			}
+			return planPanel;
+		}
+	}
+
+	CanvasGroup canvasGroup = null;
+	CanvasGroup CanvasGroup {
+		get {
+			if (canvasGroup == null) {
+				canvasGroup = GetComponent<CanvasGroup> ();
+			}
+			return canvasGroup;
+		}
+	}
+
+	Vector2 dragPosition;
+
+	#region IBeginDragHandler, IDragHandler, IEndDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData) {
+		dragPosition = transform.position - Input.mousePosition;
+		CanvasGroup.blocksRaycasts = false;
+		transform.SetParent (PlanPanel);
+	}
+
+	public void OnDrag (PointerEventData eventData) {
+		transform.position = dragPosition + (Vector2)Input.mousePosition;
+	}
+
+	public void OnEndDrag (PointerEventData eventData) {
+		CanvasGroup.blocksRaycasts = true;
+	}
+	#endregion
+}
---
+++ WorldBank-RBF/Assets/Scripts/UI/PlanTactics/new/TacticSlot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+// http://answers.unity3d.com/questions/865191/unity-new-ui-drag-and-drop.html
+
+public class TacticSlot : MonoBehaviour, IDropHandler {
+
+	 #region IDropHandler implementation
+	 public void OnDrop (PointerEventData eventData) {
+	 	Debug.Log ("heard");
+	 }
+	 #endregion
+}
