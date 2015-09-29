d4986374a5404f3d094253a8c33d0fcffa50c169
jayvachon <jaylvachon@gmail.com>
2015-09-29 17:26:13 -0400

    add lighting, update byline

---
+++ WorldBank-RBF/Assets/Scripts/Lighting/CityLighting.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CityLighting : MonoBehaviour {
+
+	public GameObject kibari;
+	public GameObject crup;
+
+	Dictionary<string, GameObject> cities;
+	Dictionary<string, GameObject> Cities {
+		get {
+			if (cities == null) {
+				cities = new Dictionary<string, GameObject> ();
+				cities.Add ("kibari", kibari);
+				cities.Add ("crup", crup);
+			}
+			return cities;
+		}
+	}
+
+	void Start () {
+		Events.instance.AddListener<ArriveInCityEvent> (OnArriveInCityEvent);
+	}
+
+	void OnArriveInCityEvent (ArriveInCityEvent e) {
+		/*if (e.City == "kibari") {
+			kibari.SetActive (true);
+		} else {
+			kibari.SetActive (false);
+		}*/
+		SetLighting (e.City);
+	}
+
+	void SetLighting (string city) {
+		foreach (var c in Cities) {
+			c.Value.SetActive (false);
+		}
+		GameObject activeCity;
+		if (Cities.TryGetValue (city, out activeCity)) {
+			// cities[city].SetActive (true);
+			activeCity.SetActive (true);
+		}
+	}
+}
---
+++ WorldBank-RBF/Assets/Scripts/Lighting/Firefly.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Light))]
+public class Firefly : MonoBehaviour {
+
+	Light light = null;
+	Light Light {
+		get {
+			if (light == null) {
+				light = GetComponent<Light> ();
+			}
+			return light;
+		}
+	}
+
+	public float minIntensity = 0.1f;
+	public float maxIntensity = 2f;
+	public float speed = 0.25f;
+	public bool randomizeSpeed = true;
+
+	bool pulsing = false;
+	float offset;
+	float mySpeed;
+	float myMaxIntensity;
+
+	void OnEnable () {
+		pulsing = true;
+		myMaxIntensity = maxIntensity * 2f;
+		mySpeed = speed + Random.Range (-0.2f, 0.2f);
+		offset = Random.Range (0f, 15f);
+		StartCoroutine (CoPulse ());
+	}
+
+	void OnDisable () {
+		pulsing = false;
+	}
+
+	IEnumerator CoPulse () {
+		while (pulsing) {
+			Light.intensity = Mathf.Lerp (
+				minIntensity, 
+				myMaxIntensity, 
+				Mathf.PingPong ((Time.time+offset)*mySpeed, 1f));
+			yield return null;
+		}
+	}
+}
