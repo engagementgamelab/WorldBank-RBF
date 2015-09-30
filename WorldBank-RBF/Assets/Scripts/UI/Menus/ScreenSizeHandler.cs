8fa2065f792baff8200cfd41aef45ef772e911fe
jayvachon <jaylvachon@gmail.com>
2015-09-30 12:04:02 -0400

    fix auto screen sizing

--- WorldBank-RBF/Assets/Scripts/UI/Menus/ScreenSizeHandler.cs
+++ WorldBank-RBF/Assets/Scripts/UI/Menus/ScreenSizeHandler.cs
@@ -5,9 +5,26 @@ public class ScreenSizeHandler : MonoBehaviour {
 
 	void Awake () {
 
+		int screenHeight = Screen.currentResolution.height;
 		Resolution[] resolutions = Screen.resolutions;
-        int lastResolution = resolutions.Length-1;
-        Screen.SetResolution (resolutions[lastResolution].width, resolutions[lastResolution].height, false);
+        Resolution targetResolution = resolutions[resolutions.Length-1];
+        int maxHeight = targetResolution.height;
+
+        int menuBarsHeight = 0;
+        #if UNITY_STANDALONE_OSX
+        menuBarsHeight = 44;
+        #elif UNITY_STANDALONE_WIN
+        menuBarsHeight = 40;
+        #endif
+
+        if (maxHeight-menuBarsHeight < screenHeight) {
+        	if (resolutions.Length >= 2)
+	        	targetResolution = resolutions[resolutions.Length-2];
+	        else
+	        	targetResolution = resolutions[0];
+        }
+
+        Screen.SetResolution (targetResolution.width, targetResolution.height, false);
 
         MenusManager.GotoScreen ("title");
 	}
